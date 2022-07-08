2ddfb1f9a7970fbc8b64efbe88fe46443cdb23ba
Benoit Rognier <benoit.rognier@edukera.com>
2022-07-08 17:22:16 +0200

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,218 @@
-import * as ts from "typescript";
+import { createPrinter, createSourceFile, factory, ListFormat, NewLineKind, NodeFlags, ScriptKind, ScriptTarget, SyntaxKind } from 'typescript';
 
-const file = ts.createSourceFile("source.ts", "", ts.ScriptTarget.ESNext, false, ts.ScriptKind.TS);
-const printer = ts.createPrinter({ newLine: ts.NewLineKind.LineFeed });
+import contract_json from '../examples/oracle.json'
+import { archetypeTypeToTsType, ContractInterface, ContractParameter, Entrypoint, FunctionParameter, importNode, StorageElement } from "./utils";
+
+const file = createSourceFile("source.ts", "", ScriptTarget.ESNext, false, ScriptKind.TS);
+const printer = createPrinter({ newLine: NewLineKind.LineFeed });
+
+
+const contract_interface : ContractInterface = contract_json
 
 // https://ts-ast-viewer.com/#
 
-const importExDecl = ts.factory.createImportDeclaration(
-  undefined,
-  undefined,
-  ts.factory.createImportClause(
-    false,
+const contractParameterToParamDecl = (fp : FunctionParameter) => {
+  return factory.createParameterDeclaration(
     undefined,
-    ts.factory.createNamespaceImport(ts.factory.createIdentifier("ex"))
-  ),
-  ts.factory.createStringLiteral("@completium/experiment-ts"),
-  undefined
-)
+    undefined,
+    undefined,
+    factory.createIdentifier(fp.name),
+    undefined,
+    archetypeTypeToTsType(fp.type),
+    undefined
+  )
+}
 
-const nodeArr = ts.factory.createNodeArray([importExDecl]);
+const storageElementToParamDecl = (se : StorageElement) => {
+  return factory.createParameterDeclaration(
+    undefined,
+    undefined,
+    undefined,
+    factory.createIdentifier(se.name),
+    factory.createToken(SyntaxKind.QuestionToken),
+    archetypeTypeToTsType(se.type),
+    undefined
+  )
+}
 
-const result = printer.printList(ts.ListFormat.MultiLine, nodeArr, file);
+const entryToMethod = (e : Entrypoint) => {
+  return factory.createMethodDeclaration(
+    undefined,
+    [factory.createModifier(SyntaxKind.AsyncKeyword)],
+    undefined,
+    factory.createIdentifier(e.name),
+    undefined,
+    undefined,
+    e.args.map(contractParameterToParamDecl).concat([
+      factory.createParameterDeclaration(
+        undefined,
+        undefined,
+        undefined,
+        factory.createIdentifier("params"),
+        undefined,
+        factory.createTypeReferenceNode(
+          factory.createIdentifier("Partial"),
+          [factory.createTypeReferenceNode(
+            factory.createIdentifier("Parameters"),
+            undefined
+          )]
+        ),
+        undefined
+      )
+    ]),
+    factory.createTypeReferenceNode(
+      factory.createIdentifier("Promise"),
+      [factory.createKeywordTypeNode(SyntaxKind.AnyKeyword)]
+    ),
+    factory.createBlock(
+      [factory.createIfStatement(
+        factory.createBinaryExpression(
+          factory.createPropertyAccessExpression(
+            factory.createThis(),
+            factory.createIdentifier("address")
+          ),
+          factory.createToken(SyntaxKind.ExclamationEqualsToken),
+          factory.createIdentifier("undefined")
+        ),
+        factory.createBlock(
+          [factory.createExpressionStatement(factory.createAwaitExpression(factory.createCallExpression(
+            factory.createIdentifier("call"),
+            undefined,
+            [
+              factory.createPropertyAccessExpression(
+                factory.createThis(),
+                factory.createIdentifier("address")
+              ),
+              factory.createStringLiteral(e.name),
+              factory.createCallExpression(
+                factory.createIdentifier(e.name+"_arg_to_mich"),
+                undefined,
+                e.args.map(x => x.name).map(x => factory.createIdentifier(x))
+              ),
+              factory.createIdentifier("params")
+            ]
+          )))],
+          true
+        ),
+        undefined
+      )],
+      true
+    )
+  )
+}
+
+const get_contract_class_node = (ci : ContractInterface) => {
+  return factory.createClassDeclaration(
+    undefined,
+    [factory.createModifier(SyntaxKind.ExportKeyword)],
+    factory.createIdentifier(ci.name.charAt(0).toUpperCase() + ci.name.slice(1)),
+    undefined,
+    undefined,
+    [
+      factory.createPropertyDeclaration(
+        undefined,
+        undefined,
+        factory.createIdentifier("address"),
+        undefined,
+        factory.createUnionTypeNode([
+          factory.createKeywordTypeNode(SyntaxKind.StringKeyword),
+          factory.createKeywordTypeNode(SyntaxKind.UndefinedKeyword)
+        ]),
+        undefined
+      ),
+      factory.createMethodDeclaration(
+        undefined,
+        undefined,
+        undefined,
+        factory.createIdentifier("get_address"),
+        undefined,
+        undefined,
+        [],
+        factory.createUnionTypeNode([
+          factory.createKeywordTypeNode(SyntaxKind.StringKeyword),
+          factory.createKeywordTypeNode(SyntaxKind.UndefinedKeyword)
+        ]),
+        factory.createBlock(
+          [factory.createReturnStatement(factory.createPropertyAccessExpression(
+            factory.createThis(),
+            factory.createIdentifier("address")
+          ))],
+          true
+        )
+      ),
+      factory.createMethodDeclaration(
+        undefined,
+        [factory.createModifier(SyntaxKind.AsyncKeyword)],
+        undefined,
+        factory.createIdentifier("deploy"),
+        undefined,
+        undefined,
+        ci.parameters.filter(x => !x.const).map(contractParameterToParamDecl).concat([
+          factory.createParameterDeclaration(
+            undefined,
+            undefined,
+            undefined,
+            factory.createIdentifier("params"),
+            undefined,
+            factory.createTypeReferenceNode(
+              factory.createIdentifier("Partial"),
+              [factory.createTypeReferenceNode(
+                factory.createIdentifier("Parameters"),
+                undefined
+              )]
+            ),
+            undefined
+          )
+        ]).concat(ci.storage.filter(x => !x.const).map(storageElementToParamDecl)),
+        undefined,
+        factory.createBlock(
+          [
+            factory.createVariableStatement(
+              undefined,
+              factory.createVariableDeclarationList(
+                [factory.createVariableDeclaration(
+                  factory.createIdentifier("address"),
+                  undefined,
+                  undefined,
+                  factory.createAwaitExpression(factory.createCallExpression(
+                    factory.createIdentifier("deploy"),
+                    undefined,
+                    [
+                      factory.createStringLiteral("./contracts/"+ci.name+".arl"),
+                      factory.createObjectLiteralExpression(
+                        [factory.createPropertyAssignment(
+                          factory.createIdentifier("publickey"),
+                          factory.createIdentifier("publickey")
+                        )],
+                        true
+                      ),
+                      factory.createIdentifier("params")
+                    ]
+                  ))
+                )],
+                NodeFlags.Const | NodeFlags.AwaitContext | NodeFlags.ContextFlags | NodeFlags.TypeExcludesFlags
+              )
+            ),
+            factory.createExpressionStatement(factory.createBinaryExpression(
+              factory.createPropertyAccessExpression(
+                factory.createThis(),
+                factory.createIdentifier("address")
+              ),
+              factory.createToken(SyntaxKind.EqualsToken),
+              factory.createIdentifier("address")
+            ))
+          ],
+          true
+        )
+      )
+    ].concat(ci.entrypoints.map(entryToMethod))
+  )
+}
+
+const nodeArr = factory.createNodeArray([
+  importNode,
+  get_contract_class_node(contract_interface)
+]);
+
+const result = printer.printList(ListFormat.MultiLine, nodeArr, file);
 console.log(result);
---
+++ src/utils.ts
@@ -0,0 +1,129 @@
+import { factory, KeywordTypeNode, SyntaxKind } from "typescript";
+
+export type ArchetypeType = {
+  "node": string
+  "name": string | null
+  "args": Array<ArchetypeType>
+}
+
+export type MichelsonType = {
+  "prim": string | null
+  "int": string | null
+  "bytes": string | null
+  "string": string | null
+  "args": Array<MichelsonType>
+  "annots": Array<string>
+  "array": Array<MichelsonType>
+}
+
+export type ContractParameter = {
+  "name": string
+  "type": ArchetypeType
+  "const": boolean
+  "default": string | null
+}
+
+export type FunctionParameter = {
+  "name": string
+  "type": ArchetypeType
+}
+
+export type Entrypoint = {
+  "name": string
+  "args": Array<FunctionParameter>
+}
+
+export type Field = {
+  "name": string
+  "type": ArchetypeType
+  "is_key": boolean
+}
+
+export type Asset = {
+  "name": string
+  "container_kind": string
+  "fields": Array<Field>
+  "container_type_michelson": MichelsonType
+  "key_type_michelson": MichelsonType
+  "value_type_michelson": MichelsonType
+}
+
+export type EnumValue = {
+  "name": string,
+  "types": Array<string> /* TODO */
+}
+
+export type Enum = {
+  "name": string
+  "constructors": Array<EnumValue>
+  "type_michelson": MichelsonType
+}
+
+export type StorageElement = {
+  "name": string
+  "type": ArchetypeType
+  "const": boolean
+}
+
+export type Record = {} /* TODO */
+
+export type Getter = {} /* TODO */
+
+export type Event = {} /* TODO */
+
+export type ContractInterface = {
+  "name" : string,
+  "parameters" : Array<ContractParameter>
+  "types": {
+    "assets": Array<Asset>
+    "enums": Array<Enum>
+    "records": Array<Record>
+    "events": Array<Event>
+  }
+  "storage": Array<StorageElement>
+  "entrypoints": Array<Entrypoint>
+  "getters": Array<Getter>
+  "errors": Array<MichelsonType>
+}
+
+export const archetypeTypeToTsType = (at: ArchetypeType) : KeywordTypeNode<any>  => {
+  switch (at.node) {
+    case "key":       return factory.createKeywordTypeNode(SyntaxKind.StringKeyword);
+    case "string":    return factory.createKeywordTypeNode(SyntaxKind.StringKeyword);
+    case "signature": return factory.createKeywordTypeNode(SyntaxKind.StringKeyword);
+    case "int":       return factory.createKeywordTypeNode(SyntaxKind.BigIntKeyword);
+    case "nat":       return factory.createKeywordTypeNode(SyntaxKind.BigIntKeyword);
+    case "asset":     return factory.createTypeReferenceNode(
+      factory.createIdentifier(at.name+"_container"),
+      undefined
+    );
+    case "asset_value":  return factory.createTypeReferenceNode(
+      factory.createIdentifier(at.args[0].name+"_value"),
+      undefined
+    );
+    case "map":    return factory.createTypeReferenceNode(
+      factory.createIdentifier("Array"),
+      [factory.createTupleTypeNode([
+        archetypeTypeToTsType(at.args[0]),
+        archetypeTypeToTsType(at.args[1])
+      ])]
+    );
+    case "tuple": return factory.createTupleTypeNode([
+      archetypeTypeToTsType(at.args[0]),
+      archetypeTypeToTsType(at.args[1])
+    ]);
+    default:       return factory.createKeywordTypeNode(SyntaxKind.StringKeyword)
+  }
+}
+
+export const importNode = factory.createImportDeclaration(
+  undefined,
+  undefined,
+  factory.createImportClause(
+    false,
+    undefined,
+    factory.createNamespaceImport(factory.createIdentifier("ex"))
+  ),
+  factory.createStringLiteral("@completium/experiment-ts"),
+  undefined
+)
