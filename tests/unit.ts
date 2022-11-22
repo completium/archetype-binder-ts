9aaf0709091f72f6eca6074c0e96021fd6de31a3
Guillaume Duhamel <guillaume.duhamel@edukera.com>
2022-11-22 10:41:52 +0100

    up

--- tests/unit.ts
+++ tests/unit.ts
@@ -8,7 +8,7 @@ describe('Test', async () => {
     const name = 'poll';
 
     // const input = fs.readFileSync("./examples/" + name + ".json");
-    const input = fs.readFileSync("./tests/contracts/json/simple_fail_invalid_condition.json");
+    const input = fs.readFileSync("./tests/contracts/json/type_asset_value_2_address.json");
     const ci = JSON.parse(input);
     const a = generate_binding(ci, { target: Target.Experiment, language: Language.Archetype, path: "./contracts/" });
     //console.log(a)
