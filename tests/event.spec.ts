a9af651e1227836c82d18f4e49c729aa0aa07357
Guillaume Duhamel <guillaume.duhamel@edukera.com>
2023-01-03 01:49:46 +0100

    up

--- tests/event.spec.ts
+++ tests/event.spec.ts
@@ -1,14 +1,13 @@
-import { expect_to_fail, get_account, set_mockup, set_mockup_now, set_quiet } from '@completium/experiment-ts'
-import { Address, Bytes, Duration, Int, Nat, Option, Rational, Tez, Ticket } from '@completium/archetype-ts-types'
+import { exec_batch, get_account, set_mockup, set_mockup_now, set_quiet } from '@completium/experiment-ts'
+import { Nat } from '@completium/archetype-ts-types'
 
-const assert = require('assert')
+import assert from 'assert'
 
 import { sample_event, e_event } from './contracts/bindings/sample_event'
 
 /* Accounts ---------------------------------------------------------------- */
 
-const alice = get_account('alice');
-const bob   = get_account('bob')
+const alice = get_account('alice')
 
 /* Endpoint ---------------------------------------------------------------- */
 
@@ -31,13 +30,27 @@ describe('[Event] Contract deployment', async () => {
 })
 
 describe('[Event] Call entry `exec`', async () => {
-  it("Call 'exec'", async () => {
+  it("Call", async () => {
     const event = new e_event(new Nat(2), "mystr")
     const res = await sample_event.exec(event, {as : alice});
     assert(res.events.length == 1)
     const payload = res.events[0].payload;
     const res_event = e_event.from_mich(payload);
     assert(res_event.equals(event))
+  })
+
+  it("Batched call", async () => {
+    const event = new e_event(new Nat(2), "mystr")
+    const param = await sample_event.get_exec_param(event, { as: alice });
+    const params = [param, param, param];
+    const res = await exec_batch(params, { as: alice })
+
+    assert(res.events.length == 3)
+    const events = res.events.map(x => { return e_event.from_mich(x.payload) })
+    events.forEach(res_event => {
+      assert(res_event.equals(event))
+    })
+
   })
 
 })
